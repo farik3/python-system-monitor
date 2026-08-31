c4665ee3e2417937124cfdffae04ff6cdfae121a
farik <farik369@gmail.com>
2026-08-31 13:20:34 +0500

    Clean repository for PC Monitor

--- for.py
+++
@@ -1,7 +0,0 @@
-total = 0
-
-for n in [2, 3, 4]:
-    if n % 2 == 0:
-        total += n
-
-print(total)
--- total = 0.py
+++
@@ -1,7 +0,0 @@
-total = 0
-
-for n in [1, 2, 4 ]:
-    total += n
-
-    if n % 2 == 0:
-        print(total)
